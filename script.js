a2c7c66e4987d2bf6ca6af87bc6104305e686164
PiusAsh <Pius1ash@gmail.com>
2022-02-17 17:59:44 +0100

    Second commit (About to fetch Api)

---
+++ script.js
@@ -0,0 +1,14 @@
+const wrapper = document.querySelector(".wrapper"),
+inputPart = wrapper.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt")
+inputField = inputPart.querySelector("input");
+
+inputField.addEventListener("keyup", e =>{
+    if(e.key == "Enter" && inputField.value != ""){
+      requestApi(inputField.value);  
+    }
+});
+
+function requestApi(city){
+    let api = ``;
+}
